3c213eb53dfb511c72f2bff1b743e9e9c9b4c964
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-14 15:42:40 +0800

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {MatButtonModule} from '@angular/material/button'
+import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
