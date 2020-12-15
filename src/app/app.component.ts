a03b0679033138de4468cef906be1e7ddcd5f7ed
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-15 13:31:34 +0800

    added ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-sample-app';
   success_msg = false
+
+  contacts = [
+    {
+      'firstName':'lewis',
+      'lastName': 'cao',
+      '_id':'0001'
+    },
+    {
+      'firstName':'Henry',
+      'lastName': 'cao',
+      '_id':'0002'
+    },
+    {
+      'firstName':'Tiffany',
+      'lastName': 'cao',
+      '_id':'0003',
+    },
+    {
+      'firstName':'Zack',
+      'lastName': 'cao',
+      '_id':'0004',
+    },
+    {
+      'firstName':'Ben',
+      'lastName': 'cao',
+      '_id':'0005',
+    },
+  ]
 }
