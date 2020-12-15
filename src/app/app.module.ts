13ae15c347961ad587accd8342d7c4414011fb10
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-15 17:29:14 +0800

    added databing intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
   ],
   imports: [
 BrowserModule,
