07555012a08e2c47b7835a9d62011bed59e8e426
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-16 22:15:11 +0800

    added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, OutletContext } from '@angular/router';
 import { HousingComponent } from './housing/housing.component';
 import { LoansComponent } from './loans/loans.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
+  //access in url localhost:4200/(loans-outlet:loans)
   {
     path: 'loans',
-    component: LoansComponent
+    component: LoansComponent,
+    outlet: 'loans-outlet'
   },
   {
     path: 'users',
-    component: UsersComponent
+    component: UsersComponent,
+    outlet: 'users-outlet'
   },
   {
-    path: 'users/user-housing',
-    component: HousingComponent
+    path: 'user-housing',
+    component: HousingComponent,
+    outlet:'users-outlet'
   },
 ];
 
