6d5a7cae238fdb7e4929585161bbb71eb911cf45
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-17 14:25:38 +0800

    route options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,33 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { HousingComponent } from './housing/housing.component';
+import { LoansComponent } from './loans/loans.component';
+import { ListProfileComponent } from './profile/list-profile/list-profile.component';
 
 const routes: Routes = [
   {
-
+    //only one component can be shown
   path:'users',
-  component: UsersComponent
+  component: UsersComponent,
+  children:[{
+    path:'loans',
+    component: LoansComponent
+  },]
 }, {
   path:'housing',
-  component:HousingComponent
-}];
+ // component:HousingComponent,
+  redirectTo: 'profile-list'
+}, 
+{
+  path:'profile-list',
+  component:ListProfileComponent
+}
+];
 
 @NgModule({
   declarations: [],
   imports: [
-  CommonModule,
+CommonModule,
   RouterModule.forRoot(routes)
   ],
   exports:[RouterModule]
